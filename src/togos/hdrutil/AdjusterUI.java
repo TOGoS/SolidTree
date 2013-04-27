71dff1aa6550a10622073b30b57fd7123c1b2900
TOGoS <togos00@gmail.com>
2013-04-26 20:44:15 -0500

    Hack up some UI thingy.

--- src/togos/hdrutil/AdjusterUI.java
+++ src/togos/hdrutil/AdjusterUI.java
@@ -2,8 +2,13 @@ package togos.hdrutil;
 
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class AdjusterUI extends Canvas
 {
@@ -11,7 +16,14 @@ public class AdjusterUI extends Canvas
 	int[] argbBuf;
 	BufferedImage bImg;
 	
-	public void repaint( Graphics g ) {
+	public synchronized void setImage( HDRImage img ) {
+		bImg = new BufferedImage( img.width, img.height, BufferedImage.TYPE_INT_ARGB );
+		argbBuf = new int[img.width*img.height];
+		this.hdrImage = img;
+		repaint();
+	}
+	
+	@Override public void paint( Graphics g ) {
 		if( hdrImage == null ) {
 			g.setColor(Color.BLACK);
 			g.fillRect(0, 0, getWidth(), getHeight());
@@ -20,5 +32,25 @@ public class AdjusterUI extends Canvas
 			bImg.setRGB(0, 0, hdrImage.width, hdrImage.height, argbBuf, 0, hdrImage.width);
 			g.drawImage(bImg, 0, 0, null);
 		}
+	}
+	
+	public static void main( String[] args ) throws Exception {
+		String dumpFilename = args[0];
+		File dumpFile = new File(dumpFilename);
+		HDRImage img = ChunkyDump.loadChunkyDump(dumpFile);
+		
+		final Frame f = new Frame("Image adjuster");
+		AdjusterUI adj = new AdjusterUI();
+		adj.setImage(img);
+		adj.setPreferredSize( new Dimension(img.width, img.height));
+		f.add(adj);
+		f.pack();
+		
+		f.setVisible(true);
+		f.addWindowListener( new WindowAdapter() {
+			@Override public void windowClosing( WindowEvent evt ) {
+				f.dispose();
+			}
+		});
 	}
 }
