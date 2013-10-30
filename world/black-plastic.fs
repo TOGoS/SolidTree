aa74bccd4e2773a86cdf0d6afaffcada0181a199
TOGoS <togos00@gmail.com>
2013-10-30 15:27:34 -0500

    Rename world scripts from <orientation>-<type> to <type>-<orientation>. Add a potted tree. Add a to-do idea.

--- world/black-plastic.fs
+++ world/black-plastic.fs
@@ -5,7 +5,7 @@
     black
     1 0.2 0 0
   make-surface-material-layer
-    0.6
+    0.8
     0.02 0.02 0.02 make-color
     black
     0 1 1 0
--- world/brick.fs
+++ world/brick.fs
@@ -3,7 +3,7 @@
     1
     0.3 0.2 0.1 make-color
     black
-    1 1 1 0
+    0 1 1 0
   make-surface-material-layer
   1
 make-surface-material make-opaque-material make-solid-material-node
