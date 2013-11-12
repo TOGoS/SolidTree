de67223985cad1c6090ca04153495c08bc7e2f0a
TOGoS <togos00@gmail.com>
2013-11-12 15:59:46 -0600

    Improve InetRenderClient performance stats output more. Previous version gave severely inaccurate results when running multithreaded.

--- src/togos/solidtree/trace/job/PerformanceCounter.java
+++ src/togos/solidtree/trace/job/PerformanceCounter.java
@@ -3,25 +3,25 @@ package togos.solidtree.trace.job;
 public class PerformanceCounter
 {
 	public volatile long sampleCount;
+	public volatile long recentSampleCount;
 	public volatile long beginTime = -1;
+	public volatile long recentBeginTime = -1;
 	public volatile long time = -1;
-	public volatile double samplesPerSecondWeightedAverage;
-	public volatile double samplesPerSecondLatest;
 	
 	public void samplesCompleted( long count ) {
 		long currentTime = System.currentTimeMillis();
 		synchronized(this) {
-			long interval = currentTime - time;
-			if( beginTime == -1 ) {
-				beginTime = currentTime;
-			}
+			if( beginTime == -1 ) beginTime = currentTime;
+			if( recentBeginTime == -1 ) recentBeginTime = currentTime;
 			sampleCount += count;
-			if( interval > 0 ) {
-				samplesPerSecondLatest = count * 1000d / interval;
-				samplesPerSecondWeightedAverage =
-					(samplesPerSecondWeightedAverage * 0.8) +
-					(samplesPerSecondLatest * 0.2);
+			// Every 20 seconds restart the recent samples/second counter
+			if( currentTime - recentBeginTime > 20*1000 ) {
+				recentBeginTime = time;
+				recentSampleCount = count;
+			} else {
+				recentSampleCount += count;
 			}
+			
 			time = currentTime;
 		}
 	}
@@ -29,7 +29,9 @@ public class PerformanceCounter
 	public static String HEADER_STRING = String.format("%15s ; %12s ; %12s", "Samples", "Recent Rate", "Overall Rate");
 	
 	public String toString() {
-		return String.format("% 15d ; % 12.2f ; % 12.2f", sampleCount, samplesPerSecondWeightedAverage,
-			(time - beginTime == 0) ? 0 : sampleCount * 1000d / (time - beginTime) );
+		return String.format("% 15d ; % 12.2f ; % 12.2f", sampleCount,
+			(time - recentBeginTime == 0) ? 0 : recentSampleCount * 1000d / (time - recentBeginTime),
+			(time - beginTime == 0) ? 0 : sampleCount * 1000d / (time - beginTime)
+		);
 	}
 }
