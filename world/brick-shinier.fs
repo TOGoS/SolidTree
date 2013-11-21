e0a69ecf5a9b2969cb692e3eec01ff7809109de2
TOGoS <togos00@gmail.com>
2013-11-21 13:20:20 -0600

    Better bricks.

---
+++ world/brick-shinier.fs
@@ -0,0 +1,9 @@
+"defs.fs" include
+
+    1
+    0.2 0.08 0.04 make-color
+    black
+    1 1 1 0
+  make-surface-material-layer
+  1
+make-surface-material make-opaque-material make-homogeneous-node
