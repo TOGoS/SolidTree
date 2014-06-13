7bebceaad16e8ebd543d78bd57b7297e1df87fe9
TOGoS <togos00@gmail.com>
2014-06-12 20:10:05 -0500

    Make grass a nicer color and not so dang shiny.

--- world/grass-material.fs
+++ world/grass-material.fs
@@ -1,9 +1,9 @@
 "defs.fs" include
 
     1
-    0.15 0.5 0.15 make-color
+    0.2 0.5 0.1 make-color
     black
-    1 0.8 0.5 0
+    0.1 0.8 0.5 0
   make-surface-material-layer
   1
 make-surface-material make-opaque-material
